2f4b9f832ee946406cdc128b80cde57e96972793
Tcomp <williamt7786@gmail.com>
2021-08-12 19:45:11 -0400

    for start of db request in DB.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,3 +24,12 @@ self.addEventListener('install', (event) => {
           self.skipWaiting()
     
 })
+
+//fetch event
+// self.addEventListener('fetch', event => {
+//     event.respondWith(
+//       caches.match(event.request).then( response => {
+//         return response || fetch(event.request);
+//       })
+//     );
+//   });
